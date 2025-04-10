ec47a7e70240938f7b502196dbffb358f19083f9
oleksiiDranenko <alexdranenko2004@icloud.com>
2025-04-10 15:48:42 +0100

    text change

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -2,7 +2,7 @@ export default function page() {
     return (
       	<main className="w-full lg:w-1/2 pt-16 px-3">
 			<div className="w-full p-3 flex justify-center font-bold text-slate-200 bg-slate-800 rounded-lg">
-                Please open this app in Telegram
+                Soon...
             </div>
       	</main>
     )
