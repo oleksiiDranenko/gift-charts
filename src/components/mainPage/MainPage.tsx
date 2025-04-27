c881196dbbc21609b9ec0141020c4158f79c8ad7
oleksiiDranenko <alexdranenko2004@icloud.com>
2025-04-27 22:21:18 +0100

    fix1

--- src/components/mainPage/MainPage.tsx
+++ src/components/mainPage/MainPage.tsx
@@ -233,7 +233,14 @@ export default function MainPage() {
                                 href={userList.length > 0 ? '/gifts-list' : '/account/settings/'}
                                 className="px-3 h-10 flex items-center bg-slate-800 rounded-lg"
                                 onClick={() => {
-                                    dispatch(setFilters({ ...filters, chosenGifts: [...userList] }));
+                                    dispatch(setFilters({
+                                        currency: filters.currency,
+                                        sort: filters.sort,
+                                        sortBy: filters.sortBy,
+                                        displayValue: filters.displayValue,
+                                        chosenGifts: userList,
+                                    }));
+                                    
                                     vibrate();
                                 }}
                             >
