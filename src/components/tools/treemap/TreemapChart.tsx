6bafbe1ad837ba624193a09e897cf13014709752
oleksiiDranenko <alexdranenko2004@icloud.com>
2025-08-20 20:14:12 +0100

    fix

--- src/components/tools/treemap/TreemapChart.tsx
+++ src/components/tools/treemap/TreemapChart.tsx
@@ -360,6 +360,7 @@ const TreemapChart: React.FC<TreemapChartProps> = ({
         const formData = new FormData();
         formData.append("file", blob, `heatmap-${Date.now()}.jpeg`);
         formData.append("chatId", chatId.toString());
+        formData.append("content", 'Here is a 1920x1080 image of a Heatmap chart!');
 
         // Send to backend
         const response = await axios.post(
@@ -515,13 +516,13 @@ const TreemapChart: React.FC<TreemapChartProps> = ({
           </button>
         </div>
       </div>
-      {/* <button
+      <button
         className="w-full flex flex-row items-center justify-center gap-x-1 text-sm h-8 rounded-t-lg border border-secondary bg-secondaryTransparent"
         onClick={downloadImage}
       >
         <Download size={16} />
         Download Heatmap as Image
-      </button> */}
+      </button>
 
       <div style={{ width: "100%", minHeight: "600px" }}>
         <canvas ref={canvasRef} />
