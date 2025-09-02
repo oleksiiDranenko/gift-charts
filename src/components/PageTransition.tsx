328d8e374f4b338835ea774da80c9f35f5fa4857
oleksiiDranenko <alexdranenko2004@icloud.com>
2025-09-03 00:11:51 +0100

    animation changed

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
-import { useState, useEffect } from 'react'
 import { Transition } from '@headlessui/react'
 
 interface Props {
@@ -10,30 +9,22 @@ interface Props {
 
 export default function PageTransition({ children }: Props) {
   const pathname = usePathname()
-  const [currentChild, setCurrentChild] = useState(children)
-  const [show, setShow] = useState(true)
-
-  useEffect(() => {
-    setShow(false) // start fade out
-    const timeout = setTimeout(() => {
-      setCurrentChild(children) // swap content
-      setShow(true) // fade in new content
-    }, 50) // short timeout
-    return () => clearTimeout(timeout)
-  }, [pathname, children])
 
   return (
-    <Transition
-      show={show}
-      appear
-      enter="transition-opacity duration-200 ease-out"
-      enterFrom="opacity-90"
-      enterTo="opacity-100"
-      leave="transition-opacity duration-200 ease-in"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-90"
-    >
-      <div className="relative w-full flex flex-row justify-center">{currentChild}</div>
-    </Transition>
+    <div className="relative w-full flex flex-row justify-center">
+      <Transition
+        key={pathname}
+        appear
+        show={true}
+        enter="transition-all ease-out duration-300"
+        enterFrom="opacity-0 translate-y-6"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition-all ease-in duration-300"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-6"
+      >
+        <div className="w-full">{children}</div>
+      </Transition>
+    </div>
   )
 }
