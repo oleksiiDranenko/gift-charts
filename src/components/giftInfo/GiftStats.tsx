166a1413656c97dc9f7f0b56143950ff74f6c0f1
oleksiiDranenko <alexdranenko2004@icloud.com>
2025-10-28 21:58:55 +0000

    fix

--- src/components/giftInfo/GiftStats.tsx
+++ src/components/giftInfo/GiftStats.tsx
@@ -1,15 +1,10 @@
-"use client";
-
 import GiftInterface from "@/interfaces/GiftInterface";
-import Image from "next/image";
 
 interface PropsInterface {
   gift: GiftInterface;
 }
 
 export default function GiftStats({ gift }: PropsInterface) {
-  const totalStarsPrice = Number(gift.starsPrice) + Number(gift.upgradePrice);
-
   const formatNumber = (number: number) => {
     const formattedNumber = new Intl.NumberFormat("de-DE").format(number);
     return formattedNumber;
@@ -88,29 +83,10 @@ export default function GiftStats({ gift }: PropsInterface) {
           <span className='text-foreground'>{"⭐ " + gift.starsPrice}</span>
         </p>
         <p className='mb-3 w-full flex justify-between'>
-          <span className='font-light'>{"Upgrade Stars Price: "}</span>
-          <span className='text-foreground'>{"⭐ " + gift.upgradePrice}</span>
-        </p>
-        <p className='mb-3 w-full flex justify-between'>
-          <span className='font-light'>{"Total Stars Price: "}</span>
-          <span className='text-foreground'>{"⭐ " + totalStarsPrice}</span>
-        </p>
-        <p className='mb-3 w-full flex justify-between'>
           <span className='font-light'>{"Total Stars Price in USD: "}</span>
-          <span className='text-foreground'>{"$ " + gift.initUsdPrice}</span>
-        </p>
-        <p className='mb-3 w-full flex justify-between'>
-          <span className='font-light'>{"Total Stars Price in TON: "}</span>
-          <div className='text-foreground flex flex-row items-center'>
-            <Image
-              alt='ton logo'
-              src='/images/toncoin.webp'
-              width={14}
-              height={14}
-              className='ml-1 mr-1'
-            />
-            {gift.initTonPrice}
-          </div>
+          <span className='text-foreground'>
+            {"$ " + gift.starsPrice * 0.015}
+          </span>
         </p>
       </div>
     </div>
