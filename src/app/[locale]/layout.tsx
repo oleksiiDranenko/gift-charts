b3b6af5113edd78a6fc3d7edadd6bd64a47d1134
oleksiiDranenko <alexdranenko2004@icloud.com>
2025-12-18 00:41:40 +0000

    fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,8 +14,6 @@ import ReactQueryProvider from "@/components/ReactQueryProvider";
 import PageTransition from "@/components/PageTransition";
 import { Metadata } from "next";
 import TgAnalytics from "@/components/TgAnalytics";
-import TelegramRedirect from "@/components/TelegramRedirect";
-import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: {
@@ -76,10 +74,6 @@ export default async function RootLayout({
           gtag('config', 'G-HFQGDBLR7K', { page_path: window.location.pathname });
         `}
       </Script>
-      <Script
-        src='https://telegram.org/js/telegram-web-app.js'
-        strategy='beforeInteractive'
-      />
 
       <html lang={locale}>
         <body
@@ -90,9 +84,6 @@ export default async function RootLayout({
                 <ReactQueryProvider>
                   <Analytics />
                   <TgAnalytics />
-                  <Suspense fallback={null}>
-                    <TelegramRedirect />
-                  </Suspense>
                   <AppInitializer>
                     <DefaultUpdate>
                       <PageTransition>{children}</PageTransition>
