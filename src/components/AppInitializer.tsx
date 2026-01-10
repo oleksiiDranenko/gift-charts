0894fb8f2be53aaa62831965652255ded6302e7d
oleksiiDranenko <alexdranenko2004@icloud.com>
2026-01-10 17:09:27 +0000

    fix

--- src/components/AppInitializer.tsx
+++ src/components/AppInitializer.tsx
@@ -138,7 +138,7 @@ export default function AppInitializer({
       const script = document.createElement("script");
       script.src = "https://telegram.org/js/telegram-widget.js?22";
       script.async = true;
-      script.setAttribute("data-telegram-login", "@gift_charts_bot");
+      script.setAttribute("data-telegram-login", "gift_charts_bot");
       script.setAttribute("data-size", "large");
       script.setAttribute("data-onauth", "onTelegramAuth(user)");
       script.setAttribute("data-request-access", "write");
