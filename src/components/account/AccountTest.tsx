8a23f2fd98fdc93997708b60e323ebcbd22d15ca
oleksiiDranenko <alexdranenko2004@icloud.com>
2026-01-24 16:18:27 +0000

    Fix

--- src/components/account/AccountTest.tsx
+++ src/components/account/AccountTest.tsx
@@ -133,7 +133,8 @@ export default function AccountTest() {
         username: "_guest",
         first_name: "Guest",
         last_name: "",
-        photo_url: "",
+        photo_url:
+          "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630",
       });
     }
   }, []);
