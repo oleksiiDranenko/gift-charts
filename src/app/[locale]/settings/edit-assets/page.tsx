0856fc31c68883ad490cd24da1c169996c725eb6
oleksiiDranenko <alexdranenko2004@icloud.com>
2026-02-03 16:23:29 +0000

    add edit assets page

---
+++ src/app/[locale]/settings/edit-assets/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import EditAssets from "@/components/account/edit-assets/EditAssets";
+
+export default function page() {
+  return (
+    <div className='w-full lg:w-[98%] pt-[0px]  pb-24'>
+      <EditAssets />
+    </div>
+  );
+}
