fb830b1f6f818c4aa9f011a89c9f2295a22495e3
oleksiiDranenko <alexdranenko2004@icloud.com>
2026-03-30 17:35:23 +0100

    add banner for epic gift

--- src/components/AddBanner.tsx
+++ src/components/AddBanner.tsx
@@ -26,7 +26,7 @@ export default function AddBanner({ className, hideable = false }: Props) {
       }`}>
       <div className='relative w-full'>
         <NoPrefetchLink
-          href='https://t.me/mutant_gifts_bot/mutantgifts?startapp=rl_f1e02fca-a966-4fe8-865f-cded194f06fb'
+          href='https://t.me/epic_gift_bot/app?startapp=cmp_GIFTCHARTSBANNER1'
           className={`${className} relative w-full lg:h-24 flex items-center justify-center rounded-2xl overflow-hidden`}
           onClick={() => {
             vibrate();
